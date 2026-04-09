98803fe3fb08eab4a365f90b2924252f80be3598
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-04-09 12:31:01 +0000

    folders

---
+++ script_boot.sh
@@ -0,0 +1,11 @@
+git branch 
+# helps you to know in wich branch you ar working on 
+git checkout 
+# Makes you swich between branches 
+mkdir proyecto
+# Create a folder which name is proyecto 
+cd /proyecto
+# You move into the folders to te folder proyecto 
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto
+# Makes you go dirrecly to te folder you want,(If you know the name of the folders to move use it)
+
