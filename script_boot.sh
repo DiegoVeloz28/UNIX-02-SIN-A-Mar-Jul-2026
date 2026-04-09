0addb8bf084dbad64db49829ede6b869e4fc31d7
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-04-09 12:44:39 +0000

    director

--- script_boot.sh
+++ script_boot.sh
@@ -8,4 +8,12 @@ cd /proyecto
 # You move into the folders to te folder proyecto 
 cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto
 # Makes you go dirrecly to te folder you want,(If you know the name of the folders to move use it)
+cd
+# Just 'cd' also takes you back Home
+cd ~
+# Goes straight back to the Home folder
+cd $HOME
+# Another way to return to the Home directory
+cd $PATH
+# This one usually fails because $PATH isn't a single directory
 
