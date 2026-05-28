963fec03a6be64f0946c3c794234f907fd591fa0
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-05-28 13:11:09 +0000

    groups

--- security2.sh
+++ security2.sh
@@ -7,3 +7,13 @@ touch ~/test_grupo_heredado.txt
 # Create a file and check which group it inherits
 ls -la ~/test_grupo_heredado.txt
 # The group is the user's primary group
+# View the current group
+id -gn
+# Display the name of the current user's primary group
+echo "Current group: $(id -gn)"
+# Print a message displaying the name of the current primary group using command substitution
+# Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+# Create an empty text file in the home directory before switching groups
+ls -la ~/antes_de_newgrp.txt
+# List all details and permissions for the newly created file to verify its group ownership
