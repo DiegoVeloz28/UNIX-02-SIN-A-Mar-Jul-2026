53eb087161857d4388e8d2f5649ae1914adc5f85
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-06-12 16:38:49 +0000

    while loop comments

--- while_loop.sh
+++ while_loop.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 SIGNAL_TO_STOP_FILE="stoploop"
+# Define the name of the trigger file that will signal the loop to terminate
 while [[ ! -f "${SIGNAL_TO_STOP_FILE}" ]]; do
-echo "The file ${SIGNAL_TO_STOP_FILE} does not yet exis
-t..."
-sleep 2
-done
+# Start a while loop that runs as long as the specified stop file does NOT exist
+echo "The file ${SIGNAL_TO_STOP_FILE} does not yet exist..."
+# Print a status message indicating the stop signal file has not been created yet
 echo "Checking again in 2 seconds..."
+# Print a notification informing the user of the upcoming re-evaluation delay
+sleep 2
+# Pause the script execution for exactly 2 seconds before checking the condition again
+done
+# Close the conditional while loop block once the file is detected
 echo "File was found! Exiting..."
+# Print a final confirmation message indicating the script is terminating successfully
