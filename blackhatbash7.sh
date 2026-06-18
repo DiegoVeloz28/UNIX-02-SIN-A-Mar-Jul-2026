4a4ba06145952555b1ada889cff9a0f291ea537c
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-06-18 12:53:14 +0000

    awk

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -5,3 +5,7 @@ ps | grep TTY
 ps | grep -i tty
 grep -v "35.237.4.214" log.txt
 grep -o "35.237.4.214" log.txt
+
+awk '{print $1,$2,$3}' log.txt
+awk '{print $1,$NF}' log.txt
+awk -F',' '{print $1}' example_csv.txt
